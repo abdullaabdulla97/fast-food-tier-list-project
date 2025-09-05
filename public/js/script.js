58d5ed370479d02e1a8396a6fb3ad4de9f4e7c8b
Abdulla Abdulla <abdulla.abdulla.salem97@gmail.com>
2025-09-05 08:46:27 -0400

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,7 @@ let logos = [] // created a variable called logos and assigned it as an empty ar
 let dragging = null // created a variable called dragging and assigned it as null to store the logo that is being currently dragged
 let offsetX = 0 // created a variable called offsetX and assigned it as 0 to store position of the logo on the x-axis when it is being dragged
 let offsetY = 0 // created a variable called offsetY and assigned it as 0 to store the position of the logo on the y-axis when it is being dragged
+let activePointerId = null // store the current pointer id (needed for touch/pointer capture)
 
 function logoRestaurant(restaurant) { // created a function called logoRestaurant with a parameter called restaurant to make a logo for the restaurant
   let image = document.createElement('img') // a new image element is made and stored it in the variable image
@@ -13,32 +14,54 @@ function logoRestaurant(restaurant) { // created a function called logoRestauran
   image.className = 'logo' // this is the image class namee
   image.setAttribute('data-name', restaurant.name) // this is to store the name of the restaurant in the image element
   image.style.position = 'relative' // the image is set at a relative position (which mean it is relative to the normal position)
-  
-  image.addEventListener('mousedown', function(e) { // added an event listener for when a user clicks on the image it will begin the dragging process
-  dragging = image // store the image that is dragged in the variable dragging
-  offsetX = e.offsetX // stores the x position of the mouse when it gets clicked in the variable offsetX
-  offsetY = e.offsetY // stores the y position of the mouse when it gets clicked in the variable offsetY
-  document.addEventListener('mousemove', handleMouseMove) // added an event listener for any mouse movement and then calls the function handleMouseMove  when that happens
-  document.addEventListener('mouseup', handleMouseUp) // added an event listener for when the mouse gets relaesd and then calls the function handleMouseUp when that happens
-  e.preventDefault() // to prevent default behaviour of the image when it is being dragged
-})
 
-return image // returns the image element containing the logo so that it can be added to the page
+  //  ensure mobile dragging works and avoid native image dragging/scrolling
+  image.style.touchAction = 'none'         // tells the browser not to handle panning/zooming while we drag
+  image.style.userSelect = 'none'          // prevent text selection while dragging
+  image.draggable = false                  // disable native HTML5 image drag
+  image.addEventListener('dragstart', e => e.preventDefault())
+
+  //  Switched from mouse events to pointer events so it works on mobile too
+  image.addEventListener('pointerdown', function(e) { // when a user touches/clicks on the image it will begin the dragging process
+    dragging = image // store the image that is dragged in the variable dragging
+
+    // compute the offset within the image based on the pointer location
+    const rect = dragging.getBoundingClientRect()
+    offsetX = e.clientX - rect.left
+    offsetY = e.clientY - rect.top
+
+    activePointerId = e.pointerId
+    // capture this pointer so we keep receiving move events even if it leaves the element
+    dragging.setPointerCapture(activePointerId)
+
+    // used pointermove/pointerup handlers (mobile + desktop)
+    document.addEventListener('pointermove', handleMouseMove, { passive: false })
+    document.addEventListener('pointerup', handleMouseUp, { once: true })
+
+    e.preventDefault() // to prevent default behaviour of the image when it is being dragged
+  })
+
+  return image // returns the image element containing the logo so that it can be added to the page
 }
 
-function handleMouseMove(e) { // this function is created to handle the mouse movement when image is being dragged
+function handleMouseMove(e) { // this function is created to handle the mouse/pointer movement when image is being dragged
   if (!dragging) { // if it is not being dragged
     return // then returns nothing and exits
+  }
+
+  dragging.style.position = 'absolute' // the images is placed at an absolute position
+
+  // place the image under the finger/cursor; clientX/Y are viewport coords, so add scroll offset
+  const pageX = e.clientX + window.scrollX
+  const pageY = e.clientY + window.scrollY
+
+  dragging.style.left = (pageX - offsetX) + 'px' // for the left position of the image, it will be the x position minus the offsetX
+  dragging.style.top  = (pageY - offsetY) + 'px' // for the top position of the image, it will be the y position minus the offsetY
+  dragging.style.zIndex = '1000' // the z-index set it to a default value of 1000, so when it is being dragged, it will be on top of the other images
+  e.preventDefault() // to prevent page from scrolling while dragging (mobile)
 }
 
-dragging.style.position = 'absolute' // the images is placed at an absolute position
-dragging.style.left = (e.pageX - offsetX) + 'px' // for the left position of the image, it will be the x position of the mouse minus the offsetX
-dragging.style.top = (e.pageY - offsetY) + 'px' // for the top position of the image, it will be the y position of the mouse minus the offsetY
-dragging.style.zIndex = '1000' // the z-index set it to a default value of 1000, so when it is being dragged, it will be on top of the other images
-e.preventDefault() // to prevent default behaviour of the image when it is being dragged
-}
-
-function handleMouseUp(e) { // this function is created to handle the mouse release
+function handleMouseUp(e) { // this function is created to handle the mouse/pointer release
   if (!dragging) { // if it is not being dragged
     return // then returns nothing and exits
   } 
@@ -47,30 +70,41 @@ function handleMouseUp(e) { // this function is created to handle the mouse rele
     let rankDiv = document.getElementById('rank-' + RANK[i]) // the id of the rank div is made by concatenation (rank- and rank name, example: rank-S)
     let rankRect = rankDiv.getBoundingClientRect() // gets the screen position of the rank div and then stores it in the variable rankRect
 
-    if (e.clientX >= rankRect.left && e.clientX <= rankRect.right && e.clientY >= rankRect.top && e.clientY <= rankRect.bottom){ // if the mouse position is inside the rank div
+    if (e.clientX >= rankRect.left && e.clientX <= rankRect.right && e.clientY >= rankRect.top && e.clientY <= rankRect.bottom){ // if the pointer position is inside the rank div
       dragging.style.position = 'static' // the position of the image will be static because it is not being dragged any longer
       dragging.style.zIndex = '' // the z-index will be empty because it is not being dragged any longer
       rankDiv.appendChild(dragging) // append the image to the rank div
-      dragging = null // set it to null becaus it is not being dragged any longer
-      document.removeEventListener('mousemove', handleMouseMove) // the event listener gets removed for mouse movement
-      document.removeEventListener('mouseup', handleMouseUp) // the event listener gets removed for mouse release
-      e.preventDefault() // to prevent default behaviour of the image when it is being dragged
+
+      // cleanup listeners/capture
+      if (activePointerId !== null) {
+        try { 
+          dragging.releasePointerCapture(activePointerId) 
+        } catch (_) {}
+      }
+      dragging = null // set it to null because it is not being dragged any longer
+      document.removeEventListener('pointermove', handleMouseMove) // remove move handler
+      e.preventDefault() // prevent any default behavior
       return // return nothing and then exits
     }
   }
 
   let list = document.getElementById('logolist') // created a variable called list to store the list of logos 
   let listRect = list.getBoundingClientRect() // gets the screen position of the list and then stores in the variable listRect
-  if (e.clientX >= listRect.left && e.clientX <= listRect.right && e.clientY >= listRect.top && e.clientY <= listRect.bottom) { // if the mouse position is inside the list
+  if (e.clientX >= listRect.left && e.clientX <= listRect.right && e.clientY >= listRect.top && e.clientY <= listRect.bottom) { // if the pointer position is inside the list
     dragging.style.position = 'static' // the position of the image will be static because it is not being dragged any longer
     dragging.style.zIndex = '' // the z-index will be empty because it not being dragged any longer
     list.appendChild(dragging) // append the image to the list
   }
-  dragging = null // set it to null because it is not being dragged any longer
-  document.removeEventListener('mousemove', handleMouseMove) // the event listener gets removed for mouse movement
-  document.removeEventListener('mouseup', handleMouseUp) // the event listener gets removed for mouse release
 
-  e.preventDefault() // to prevent default behaviour of the image when it is being dragged 
+  // final cleanup when not dropped on a rank or after list append
+  if (activePointerId !== null && dragging) {
+    try { 
+      dragging.releasePointerCapture(activePointerId) 
+    } catch (_) {}
+  }
+  dragging = null // set it to null because it is not being dragged any longer
+  document.removeEventListener('pointermove', handleMouseMove) // remove move handler
+  e.preventDefault() // to prevent default behaviour 
 }
 
 function showLogos() { // this function is created show the logos of the fast food restaurants 
